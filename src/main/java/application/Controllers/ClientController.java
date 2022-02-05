3fcb7bb989727946c0bc466580111af131298ebc
Daaizo <danielcharlak@wp.pl>
2022-02-06 00:42:13 +0100

    client scene now when cart icon is clicked there is name of product and name of user that clicked it, next step will be inserting as a query to database

--- src/main/java/application/Controllers/ClientController.java
+++ src/main/java/application/Controllers/ClientController.java
@@ -4,8 +4,6 @@ import application.Main;
 import javafx.beans.binding.Bindings;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
@@ -51,6 +49,7 @@ public class ClientController extends Controller {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         categoryPickingPane.setVisible(true);
         ebooksAnchorPane.setVisible(false);
         gamesAnchorPane.setVisible(false);
@@ -61,7 +60,7 @@ public class ClientController extends Controller {
     }
 
 
-    private Callback createButtonInTableView(EventHandler eventHandler, String buttonIconName, String buttonText) {
+    private Callback createButtonInTableView(String buttonIconName, String buttonText) {
         return new Callback<TableColumn<ProductTable, String>, TableCell<ProductTable, String>>() {
             @Override
             public TableCell call(final TableColumn<ProductTable, String> param) {
@@ -75,10 +74,27 @@ public class ClientController extends Controller {
 
                         button.setGraphic(iconPath(buttonIconName));
                         button.setBackground(Background.EMPTY);
-                        button.setOnAction(eventHandler);
+                        button.setOnAction(actionEvent -> {
+                            ProductTable product = getTableView().getItems().get(getIndex());
+                            if (buttonIconName.compareTo("star.png") == 0) {
+                                System.out.print(" gwiazdka ");
+                            } else
+                                System.out.print(" koszyk ");
+                            System.out.println(product.getProductName() + product.getProductPrice() + currentUserName);
+                        });
+                        button.setOnMouseEntered(mouseEvent -> {
+                                    button.setStyle("-fx-background-color : red");
+
+                                }
+
+                        );
+                        button.setOnMouseExited(mouseEvent -> {
+                            button.setStyle("-fx-background-color : transparent");
+                        });
                         setGraphic(button);
                     }
                 };
+
             }
         };
     }
@@ -87,8 +103,8 @@ public class ClientController extends Controller {
         ebooksNameColumn.setCellValueFactory(new PropertyValueFactory<>("productName"));
         ebooksPriceColumn.setCellValueFactory(new PropertyValueFactory<>("productPrice"));
         ebooksSubcategoryColumn.setCellValueFactory(new PropertyValueFactory<>("productSubcategory"));
-        ebooksStarButtonColumn.setCellFactory(createButtonInTableView(starOnAction(), "star.png", "add to favourite"));
-        ebooksCartButtonColumn.setCellFactory(createButtonInTableView(cartOnAction(), "add_cart.png", "add to cart"));
+        ebooksStarButtonColumn.setCellFactory(createButtonInTableView("star.png", "add to favourite"));
+        ebooksCartButtonColumn.setCellFactory(createButtonInTableView("add_cart.png", "add to cart"));
         ebooksCartButtonColumn.setStyle("  -fx-padding: 18 5 5 5px;");
         ebooksStarButtonColumn.setStyle("  -fx-padding: 18 5 5 5px;");
         ebooksTableView.setItems(list);
@@ -97,25 +113,7 @@ public class ClientController extends Controller {
 
     }
 
-    private EventHandler starOnAction() {
-        EventHandler eventHandler = new EventHandler() {
-            @Override
-            public void handle(Event event) {
-                System.out.println("kliknieto gwiazdke");
-            }
-        };
-        return eventHandler;
-    }
 
-    private EventHandler cartOnAction() {
-        EventHandler eventHandler = new EventHandler() {
-            @Override
-            public void handle(Event event) {
-                System.out.println("kliknieto cart");
-            }
-        };
-        return eventHandler;
-    }
 
     void displayEbooks() throws SQLException {
         Connection con = Main.connectToDatabase();
@@ -130,8 +128,8 @@ public class ClientController extends Controller {
         gamesNameColumn.setCellValueFactory(new PropertyValueFactory<>("productName"));
         gamesPriceColumn.setCellValueFactory(new PropertyValueFactory<>("productPrice"));
         gamesSubcategoryColumn.setCellValueFactory(new PropertyValueFactory<>("productSubcategory"));
-        gamesStarButtonColumn.setCellFactory(createButtonInTableView(starOnAction(), "star.png", "add to favourite"));
-        gamesCartButtonColumn.setCellFactory(createButtonInTableView(cartOnAction(), "add_cart.png", "add to cart"));
+        gamesStarButtonColumn.setCellFactory(createButtonInTableView("star.png", "add to favourite"));
+        gamesCartButtonColumn.setCellFactory(createButtonInTableView("add_cart.png", "add to cart"));
         gamesCartButtonColumn.setStyle("  -fx-padding: 18 5 5 5px;");
         gamesStarButtonColumn.setStyle("  -fx-padding: 18 5 5 5px;");
         gamesTableView.setItems(list);
