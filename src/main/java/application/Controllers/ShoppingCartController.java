97bf34879b5c20712a73adc592e066d573ab3778
Daaizo <danielcharlak@wp.pl>
2022-02-09 22:38:55 +0100

    Shopping Cart huge update, now each row has 3 buttons, one to increment quantity, one to decrement quantity, and one to delete whole position. In database there is new trigger which deletes position with 0 quantity. There are confirmation alerts when trying to delete whole item. There is information when cart is empty instead of "there is no context in table".

--- src/main/java/application/Controllers/ShoppingCartController.java
+++ src/main/java/application/Controllers/ShoppingCartController.java
@@ -1,24 +1,30 @@
 package application.Controllers;
 
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import users.Client;
 import users.Product;
 import users.ProductTable;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class ShoppingCartController extends Controller {
 
     @FXML
-    private TableColumn<ProductTable, String> cartNameColumn, cartPriceColumn, cartValueColumn;
+    private TableColumn<ProductTable, String> cartNameColumn, cartPriceColumn, cartValueColumn, plusButtonColumn, minusButtonColumn, deleteButtonColumn;
     @FXML
     private TableColumn<ProductTable, Integer> cartQuantityColumn;
     @FXML
     private TableView<ProductTable> cartTableView;
+    @FXML
+    private Label emptyCart;
 
     @FXML
     public void initialize() {
@@ -35,10 +41,16 @@ public class ShoppingCartController extends Controller {
     void displayProducts() throws SQLException {
         checkConnectionWithDb();
         ResultSet products = Product.getProductFromCart(getConnection(), CURRENT_USER_LOGIN);
-
-        assert products != null;
         ObservableList<ProductTable> listOfProducts = ProductTable.getProductsFromShoppingCart(products);
-        fillShoppingCartColumnsWithData(listOfProducts);
+        if (listOfProducts.isEmpty()) {
+            displayLabelWithGivenText(emptyCart, "SHOPPING CART IS EMPTY");
+            cartTableView.setVisible(false);
+        } else {
+            cartTableView.setVisible(true);
+            fillShoppingCartColumnsWithData(listOfProducts);
+            showOnlyRowsWithData(cartTableView, listOfProducts);
+        }
+
     }
 
     private void fillShoppingCartColumnsWithData(ObservableList<ProductTable> list) {
@@ -46,8 +58,91 @@ public class ShoppingCartController extends Controller {
         cartPriceColumn.setCellValueFactory(new PropertyValueFactory<>("productPrice"));
         cartQuantityColumn.setCellValueFactory(new PropertyValueFactory<>("productQuantity"));
         cartValueColumn.setCellValueFactory(new PropertyValueFactory<>("productTotalValue"));
+        plusButtonColumn.setCellFactory(buttonCreation -> plusButtonClicked());
+        minusButtonColumn.setCellFactory(buttonCreation -> minusButtonClicked());
+        deleteButtonColumn.setCellFactory(buttonCreation -> deleteButtonClicked());
         showOnlyRowsWithData(cartTableView, list);
 
+
+    }
+
+    private void reloadTableView(TableView<ProductTable> tableView) throws SQLException {
+        tableView.getItems().clear();
+        displayProducts();
+    }
+
+    private void confirmationAlert(String productName, String productQuantity) throws SQLException {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText("DELETING PRODUCT FROM CART ");
+        alert.setContentText("Do you want to delete " + productQuantity + " " + productName + " from cart");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            Client.setQuantityOfProduct(CURRENT_USER_LOGIN, productName, "-quantity", getConnection());
+            // there is a trigger in database which deletes products from cart when quantity is equal 0
+        }
+    }
+
+    private ClientController.ButtonInsideTableColumn plusButtonClicked() {
+        ClientController.ButtonInsideTableColumn button = new ClientController().new ButtonInsideTableColumn("plus.png", "");
+        EventHandler<MouseEvent> buttonClicked = mouseEvent -> {
+            String productName = button.getProductName();
+            System.out.println(productName + CURRENT_USER_LOGIN);
+            try {
+                checkConnectionWithDb();
+                Client.setQuantityOfProduct(CURRENT_USER_LOGIN, productName, "+1", getConnection());
+                reloadTableView(cartTableView);
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        };
+        button.setEventHandler(buttonClicked);
+        return button;
+    }
+
+    private boolean isQuantityEqualZero(String productName, Connection connection) throws SQLException {
+        return Client.getQuantityOfProductInCart(CURRENT_USER_LOGIN, productName, connection) == 0;
+    }
+
+    private ClientController.ButtonInsideTableColumn minusButtonClicked() {
+        ClientController.ButtonInsideTableColumn button = new ClientController().new ButtonInsideTableColumn("minus.png", "");
+        EventHandler<MouseEvent> buttonClicked = mouseEvent -> {
+            String productName = button.getProductName();
+            try {
+                checkConnectionWithDb();
+                Client.setQuantityOfProduct(CURRENT_USER_LOGIN, productName, "-1", getConnection());
+                if (isQuantityEqualZero(productName, getConnection())) {
+                    confirmationAlert(productName, "");
+                }
+
+                reloadTableView(cartTableView);
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+
+        };
+        button.setEventHandler(buttonClicked);
+        button.setFxStyle("-fx-alignment : center_left;");
+        return button;
+    }
+
+    private ClientController.ButtonInsideTableColumn deleteButtonClicked() {
+        ClientController.ButtonInsideTableColumn button = new ClientController().new ButtonInsideTableColumn("delete.png", "delete from cart");
+        EventHandler<MouseEvent> buttonClicked = mouseEvent -> {
+            String productName = button.getProductName();
+            try {
+                confirmationAlert(productName, Client.getQuantityOfProductInCart(CURRENT_USER_LOGIN, productName, getConnection()) + "");
+                reloadTableView(cartTableView);
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+
+        };
+
+        button.setEventHandler(buttonClicked);
+        return button;
     }
 
 
