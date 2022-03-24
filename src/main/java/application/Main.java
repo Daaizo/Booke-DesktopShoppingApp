69b9eb8c94085c80bcf5fdfa3f0037562448aa9d
Daaizo <danielcharlak@wp.pl>
2022-03-24 11:56:29 +0100

    start scene error that informs about connection with database close application on click

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -3,16 +3,19 @@ package application;
 
 import dataBase.SqlConnection;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.util.Objects;
+import java.util.Optional;
 
 
 public class Main extends Application {
@@ -20,9 +23,12 @@ public class Main extends Application {
         SqlConnection sqlConnection = SqlConnection.createInstance();
         Connection sqlQueryConnection = sqlConnection.getConnection();
         while (sqlQueryConnection == null) {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Connection to data base failed. Reconnect and try again.");
-            alert.showAndWait();
-
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Connection to data base failed. Reconnect and try again.", ButtonType.OK, ButtonType.CLOSE);
+            Optional<ButtonType> alertButton = alert.showAndWait();
+            if (alertButton.isPresent() && alertButton.get() == ButtonType.CLOSE) {
+                Platform.exit();
+                break;
+            }
             SqlConnection.createInstance();
             sqlQueryConnection = sqlConnection.getConnection();
         }
@@ -33,7 +39,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             createWindow(primaryStage);
-            Connection connection = connectToDatabase();
+            connectToDatabase();
 
         } catch (Exception e) {
             System.out.println(e.getMessage() + "error from start method");
