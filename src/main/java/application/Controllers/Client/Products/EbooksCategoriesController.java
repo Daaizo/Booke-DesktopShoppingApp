ebc4eb852936c723daf5b14c4c88051e3ddf1667
Daaizo <danielcharlak@wp.pl>
2022-04-01 23:57:26 +0200

    e-books scene All the buttons are placed in the grid panel to look nice and have equal spacing. Instead of creating a new FXML with the same process of creating a new table view, filling it with data from the database, giving it a new title, creating buttons, etc. there are methods that do this automatically by just providing the name of the subcategory. This works so that each new table has the same values name price number of orders and value if it is in favorites

--- src/main/java/application/Controllers/Client/Products/EbooksCategoriesController.java
+++ src/main/java/application/Controllers/Client/Products/EbooksCategoriesController.java
@@ -1,64 +1,143 @@
 package application.Controllers.Client.Products;
 
-import application.Controllers.Controller;
+import application.Controllers.Client.ClientStartSceneController;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import users.Product;
+import users.ProductTable;
 
-public class EbooksCategoriesController extends Controller {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EbooksCategoriesController extends ClientStartSceneController {
     @FXML
     private Pane ebooksCategoryPane;
 
-
     @FXML
-    private Button fantasy, shotCategoryImage;
+    private Button fantasy, science, sciFi, crime, allEbooks;
+    @FXML
+    private TableView<ProductTable> productsTableView;
 
     @FXML
     public void initialize() {
-        /*
-
-        ImageView shot = setImageFromIconsFolder("/CategoryIcons/science-book.png");
-        shotCategoryImage.setLayoutX(400);
-        shotCategoryImage.setLayoutY(100);
-        shot.setLayoutX(700);
-        shot.setLayoutY(100);
-        fantasyCategoryImage.setLayoutX(100);
-        fantasyCategoryImage.setLayoutY(100);
-        fantasyCategoryImage.setVisible(true);
-        shotCategoryImage.setVisible(true);
-        ebooksCategoryPane.getChildren().addAll(fantasyCategoryImage, shotCategoryImage, shot);
-
-         */
+        initializeButtons();
         prepareAllImages();
-        setActionsOnclick();
-        getCategoryImagesAndPlaceThemInGrid();
+        setButtonsActions();
+        getCategoryButtonsAndPlaceThemInGrid();
     }
 
     private void prepareAllImages() {
+        fantasy.setGraphic(setImageFromIconsFolder("/CategoryIcons/fantasy.png"));
+        science.setGraphic(setImageFromIconsFolder("/CategoryIcons/science.png"));
+        sciFi.setGraphic(setImageFromIconsFolder("/CategoryIcons/sci-fi.png"));
+        crime.setGraphic(setImageFromIconsFolder("/CategoryIcons/crime.png"));
+
+    }
+
+    private void initializeButtons() {
+        String cssClassName = "categoryImages";
         fantasy = new Button();
-        shotCategoryImage = new Button();
-        fantasy.setGraphic(setImageFromIconsFolder("/CategoryIcons/fairy-tale.png"));
-        shotCategoryImage.setGraphic(setImageFromIconsFolder("/CategoryIcons/weapons.png"));
-        fantasy.getStyleClass().add("categoryImages");
-        shotCategoryImage.getStyleClass().add("categoryImages");
+        science = new Button();
+        sciFi = new Button();
+        crime = new Button();
+        fantasy.getStyleClass().add(cssClassName);
+        science.getStyleClass().add(cssClassName);
+        sciFi.getStyleClass().add(cssClassName);
+        crime.getStyleClass().add(cssClassName);
+        createAllEbooksButton();
     }
 
-    private void setActionsOnclick() {
-        fantasy.setOnMouseClicked(mouseEvent -> System.out.println("klik fantasy"));
+    private void createAllEbooksButton() {
+        allEbooks = new Button("All e-books");
+        allEbooks.getStyleClass().add("CategoryPickingButtons");
+        allEbooks.setMaxWidth(1000);
+        allEbooks.setPrefHeight(100);
+    }
+
+    private void setButtonsActions() {
+        allEbooks.setOnAction(event -> loadPane("allEbooksPaneGUI.fxml"));
+        fantasy.setOnMouseClicked(mouseEvent -> {
+            prepareNewPane("fantasy books");
+        });
 
     }
 
-    private void getCategoryImagesAndPlaceThemInGrid() {
+    private void getCategoryButtonsAndPlaceThemInGrid() {
         GridPane grid = new GridPane();
-        int numberOfImagesInRow = 3;
         double padding = 70;
         grid.setPadding(new Insets(padding));
         grid.setHgap(padding);
         grid.setVgap(padding);
         grid.add(fantasy, 0, 0);
-        grid.add(shotCategoryImage, 1, 0);
+        grid.add(science, 1, 0);
+        grid.add(sciFi, 2, 0);
+        grid.add(crime, 3, 0);
+        grid.add(allEbooks, 0, 1, 4, 1);
+        grid.setLayoutY(30);
+        grid.setLayoutX(20);
         ebooksCategoryPane.getChildren().add(grid);
+        GridPane.setFillWidth(allEbooks, true);
+
+    }
+
+    private void createSubSceneTitle(String text) {
+        Label title = new Label();
+        title.setId("titleLabel");
+        title.setText(text);
+        title.setLayoutY(anchor.getWidth() / 2);
+        title.setLayoutX(10);
+    }
+
+    private void createTableView() {
+        productsTableView = new TableView<>();
+        productsTableView.setPlaceholder(new Label("There are no ordered products "));
+        productsTableView.setPrefWidth(900);
+        TableColumn<ProductTable, String> productName = new TableColumn<>("name");
+        productName.setMinWidth(200);
+        TableColumn<ProductTable, String> productPrice = new TableColumn<>("price");
+        productPrice.setPrefWidth(200);
+        TableColumn<ProductTable, String> cartButtonColumn = new TableColumn<>();
+        TableColumn<ProductTable, String> starButtonColumn = new TableColumn<>();
+        starButtonColumn.setPrefWidth(230);
+        cartButtonColumn.setPrefWidth(230);
+        productsTableView.getColumns().addAll(productName, productPrice, cartButtonColumn, starButtonColumn);
+        productName.setCellValueFactory(new PropertyValueFactory<>("productName"));
+        productPrice.setCellValueFactory(new PropertyValueFactory<>("productPrice"));
+        starButtonColumn.setCellValueFactory(buttonInsideCell -> buttonInsideCell.getValue().isProductFavouriteProperty());
+        starButtonColumn.setCellFactory(buttonInsideCell -> createStarButtonInsideTableView());
+        cartButtonColumn.setCellFactory(buttonInsideCell -> createCartButtonInsideTableView());
+        ebooksCategoryPane.getChildren().add(productsTableView);
+    }
+
+    private void displayProductsFromCategory(String subcategoryName) throws SQLException {
+        ResultSet products = Product.getProductsFromSubcategoryAndInformationIfProductIsInUsersFavouriteFromDatabase(getConnection(), CURRENT_USER_LOGIN, subcategoryName);
+        ObservableList<ProductTable> listOfProducts = ProductTable.getProductsFromSubcategoryWithInformationIfTheyAreInUsersFavourite(products);
+        productsTableView.setItems(listOfProducts);
+        prepareTableView(productsTableView, (TableColumn<?, String>) productsTableView.getColumns().get(1));
+        productsTableView.setMaxHeight(250);
+    }
+
+    private void prepareNewPane(String subcategoryName) {
+        clearPane(ebooksCategoryPane);
+        goBackButton.setOnAction(event -> loadPane("ebooksCategoriesGUI.fxml"));
+        getProductsFromDbIntoTableView(subcategoryName);
+    }
+
+    private void getProductsFromDbIntoTableView(String subcategoryName) {
+        checkConnectionWithDb();
+        createTableView();
+        try {
+            displayProductsFromCategory("fantasy books");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
