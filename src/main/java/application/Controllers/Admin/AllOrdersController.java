f9ba675535814d547ff58ff7a0b476d31efb48b7
Daaizo <danielcharlak@wp.pl>
2022-04-09 21:59:29 +0200

    Admin orders now admin can see all orders with basic information like id, customer id, status, dates and total value

--- src/main/java/application/Controllers/Admin/AllOrdersController.java
+++ src/main/java/application/Controllers/Admin/AllOrdersController.java
@@ -1,11 +1,52 @@
 package application.Controllers.Admin;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import users.Order;
+import users.OrderTable;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AllOrdersController extends AdminStartSceneController {
+    @FXML
+    private TableColumn<OrderTable, String> ordersAcceptColumn, ordersDateColumn, ordersPriceColumn,
+            ordersDeliveryDateColumn, ordersDetailButtonColumn, ordersPaymentMethodColumn, ordersStatusColumn;
+    @FXML
+    private TableColumn<OrderTable, Integer> ordersIdColumn;
+    @FXML
+    private TableView<OrderTable> ordersTableView;
 
     @FXML
     private void initialize() {
+        try {
+            displayOrders();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    void displayOrders() throws SQLException {
+        checkConnectionWithDb();
+        ResultSet allOrdersFromDb = Order.getAllOrders(getConnection());
+        ObservableList<OrderTable> orders = OrderTable.getAllOrdersFromDb(allOrdersFromDb);
+        fillOrdersColumnsWithData(orders);
+    }
+
+    private void fillOrdersColumnsWithData(ObservableList<OrderTable> list) {
+        ordersIdColumn.setCellValueFactory(new PropertyValueFactory<>("orderNumber"));
+        ordersDateColumn.setCellValueFactory(new PropertyValueFactory<>("orderDate"));
+        ordersDeliveryDateColumn.setCellValueFactory(new PropertyValueFactory<>("orderDeliveryDate"));
+        ordersPriceColumn.setCellValueFactory(new PropertyValueFactory<>("orderTotalValue"));
+        ordersPaymentMethodColumn.setCellValueFactory(new PropertyValueFactory<>("orderPaymentMethodName"));
+        ordersStatusColumn.setCellValueFactory(new PropertyValueFactory<>("orderStatusName"));
+
+        ordersAcceptColumn.setCellValueFactory(new PropertyValueFactory<>(""));
+        ordersDetailButtonColumn.setCellValueFactory(new PropertyValueFactory<>(""));
+        ordersTableView.setItems(list);
+        prepareTableView(ordersTableView, ordersPriceColumn);
     }
 }
