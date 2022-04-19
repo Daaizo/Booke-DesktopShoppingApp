c2710d443de050ab0ea01dfd780d5b7bc8ec8e48
Daaizo <danielcharlak@wp.pl>
2022-04-19 21:50:50 +0200

    Admin scene update admin can see each of order details and change payment method, cancel it etc.

--- src/main/java/application/Controllers/Admin/AllOrdersController.java
+++ src/main/java/application/Controllers/Admin/AllOrdersController.java
@@ -1,27 +1,43 @@
 package application.Controllers.Admin;
 
+import application.Controllers.ButtonInsideTableColumn;
+import application.Controllers.Client.Account.ClientOrderDetails;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import users.Order;
 import users.OrderTable;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AllOrdersController extends AdminStartSceneController {
     @FXML
+    private Pane ordersPane, orderDetailsPane;
+    @FXML
     private TableColumn<OrderTable, String> ordersAcceptColumn, ordersDateColumn, ordersPriceColumn,
-            ordersDeliveryDateColumn, ordersDetailButtonColumn, ordersPaymentMethodColumn, ordersStatusColumn;
+            ordersDeliveryDateColumn, ordersDetailButtonColumn, orderDetails, ordersStatusColumn;
     @FXML
     private TableColumn<OrderTable, Integer> ordersIdColumn;
     @FXML
     private TableView<OrderTable> ordersTableView;
+    private Button goBackButton;
 
     @FXML
     private void initialize() {
+        createGoBackButton();
         try {
             displayOrders();
         } catch (SQLException e) {
@@ -36,19 +52,152 @@ public class AllOrdersController extends AdminStartSceneController {
         fillOrdersColumnsWithData(orders);
     }
 
+    private void createGoBackButton() {
+        goBackButton = createGoBackButton(event -> {
+            orderDetailsPane.setVisible(false);
+            ordersPane.setVisible(true);
+            goBackButton.setVisible(false);
+        });
+        goBackButton.fire();
+    }
+
     private void fillOrdersColumnsWithData(ObservableList<OrderTable> list) {
         ordersIdColumn.setCellValueFactory(new PropertyValueFactory<>("orderNumber"));
         ordersDateColumn.setCellValueFactory(new PropertyValueFactory<>("orderDate"));
         ordersDeliveryDateColumn.setCellValueFactory(new PropertyValueFactory<>("orderDeliveryDate"));
         ordersPriceColumn.setCellValueFactory(new PropertyValueFactory<>("orderTotalValue"));
-        ordersPaymentMethodColumn.setCellValueFactory(new PropertyValueFactory<>("orderPaymentMethodName"));
         ordersStatusColumn.setCellValueFactory(new PropertyValueFactory<>("orderStatusName"));
+
+        orderDetails.setCellFactory(orderTableStringTableColumn -> createOrderIdButton());
 
         ordersAcceptColumn.setCellValueFactory(buttonInsideCell -> buttonInsideCell.getValue().orderStatusNameProperty());
         ordersAcceptColumn.setCellFactory(orderTableStringTableColumn -> createStatusButtons());
-        ordersDetailButtonColumn.setCellValueFactory(new PropertyValueFactory<>(""));
+        ordersDetailButtonColumn.setCellFactory(orderTableStringTableColumn -> createOrderIdButton());
         ordersTableView.setItems(list);
         prepareTableView(ordersTableView, ordersPriceColumn);
     }
 
+
+    EventHandler<MouseEvent> buttonInsideTableViewClicked(ButtonInsideOrdersTableView button) {
+        return mouseEvent -> {
+            int orderNumber = button.getRowId().getOrderNumber();
+            checkConnectionWithDb();
+            System.out.println(orderNumber);
+
+        };
+    }
+
+    ButtonInsideOrdersTableView createStatusButtons() {
+        Button acceptButton = new Button("approve");
+        Button cancelButton = new Button("cancel");
+        cancelButton.setOnMouseClicked(mouseEvent -> System.out.println("tralala"));
+
+        return new ButtonInsideOrdersTableView(acceptButton, cancelButton);
+    }
+
+    ButtonInsideTableColumn<OrderTable, String> createOrderIdButton() {
+        ButtonInsideTableColumn<OrderTable, String> button = new ButtonInsideTableColumn<>("", "details");
+        button.setEventHandler(createActionOnClick(button));
+        button.setCssId("orderDetailsButton");
+        return button;
+    }
+
+    private EventHandler<MouseEvent> createActionOnClick(ButtonInsideTableColumn<OrderTable, String> button) {
+        return mouseEvent -> {
+            FXMLLoader loader = createLoaderWithCustomController(button);
+            try {
+                displayOrderDetails(loader);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+    }
+
+    private FXMLLoader createLoaderWithCustomController(ButtonInsideTableColumn<OrderTable, String> button) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/FXML/ClientSceneFXML/ClientAccountFXML/clientOrderDetailsGUI.fxml"));
+        ClientOrderDetails clientOrderDetailsController = new ClientOrderDetails();
+        clientOrderDetailsController.setOrder(button.getRowId());
+        clientOrderDetailsController.setOrderNumber(button.getRowId().getOrderNumber());
+        clientOrderDetailsController.setAllOrdersPane(ordersPane);
+        clientOrderDetailsController.setGoBackButton(goBackButton);
+        loader.setController(clientOrderDetailsController);
+        return loader;
+    }
+
+    private void displayOrderDetails(FXMLLoader loader) throws IOException {
+        orderDetailsPane.getChildren().clear();
+        ordersPane.setVisible(false);
+        orderDetailsPane.getChildren().add(loader.load());
+        goBackButton.setVisible(true);
+        orderDetailsPane.setVisible(true);
+    }
+
+    static class ButtonInsideOrdersTableView extends ButtonInsideTableColumn<OrderTable, String> {
+        private Button approveButton;
+        private Button cancelButton;
+
+        public ButtonInsideOrdersTableView(Button approveButton, Button cancelButton) {
+            this.cancelButton = cancelButton;
+            this.approveButton = approveButton;
+        }
+
+        public ButtonInsideOrdersTableView(String buttonText) {
+            button.fire();
+            button.setText(buttonText);
+            setGraphic(button);
+        }
+
+
+        protected ImageView setIconFromAdminIconsFolder(String iconName) {
+            return new ImageView(getClass().getResource("/application/Icons/AdminIcons/") + iconName);
+        }
+
+        private void setButtonsBackground() {
+            cancelButton.setBackground(Background.EMPTY);
+            approveButton.setBackground(Background.EMPTY);
+        }
+
+        private void setButtonImages() {
+            approveButton.setGraphic(setIconFromAdminIconsFolder("approve.png"));
+            cancelButton.setGraphic(setIconFromAdminIconsFolder("cancel.png"));
+        }
+
+
+        @Override
+        protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+            if (empty) {
+                setGraphic(null);
+                setText(null);
+            } else {
+
+                if (item != null) {
+                    switch (item) {
+                        case "In progress" -> {
+                            setButtonsBackground();
+                            setButtonImages();
+                            HBox pane = new HBox(approveButton, cancelButton);
+                            setGraphic(pane);
+                            //   setOnMouseClicked(deleteFromFavouriteClicked(getRowId()));
+                            setOnMouseEntered(event -> setCursor(Cursor.HAND));
+                            setOnMouseExited(event -> setCursor(Cursor.DEFAULT));
+
+                        }
+                        case "Canceled" -> {
+                            setGraphic(setIconFromAdminIconsFolder("rejected.png"));
+                        }
+                        case "Sent" -> {
+                            setGraphic(setIconFromAdminIconsFolder("approved.png"));
+                        }
+                        default -> {
+                            button.setText("-");
+                        }
+                    }
+                    approveButton.getStyleClass().add("clientTableviewButtons");
+                    cancelButton.getStyleClass().add("clientTableviewButtons");
+                }
+            }
+        }
+
+    }
 }
