f80f3f04002c1fd057813e8419fbbe39cf020515
Daaizo <danielcharlak@wp.pl>
2022-05-21 21:58:35 +0200

    feat: admin can add new user
    While adding new user at first it is checked if :
    1. all fields are not empty
        if any  is -> alert
    2. if login is unique
       if is not -> alert
    3. Confirmation alert + re-entering admin password to commit changes
    4. all fields are cleared and table is refreshed
    All tested.

--- src/main/java/application/Controllers/Admin/AllUserController.java
+++ src/main/java/application/Controllers/Admin/AllUserController.java
@@ -2,10 +2,7 @@ package application.Controllers.Admin;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import users.Client;
@@ -14,6 +11,7 @@ import users.ClientTable;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class AllUserController extends AdminStartSceneController {
     @FXML
@@ -36,17 +34,72 @@ public class AllUserController extends AdminStartSceneController {
     private void createButtons() {
         Button addNewUserButton = new Button("Add new user");
         addNewUserButton.getStyleClass().add("OrangeButtons");
-        ArrayList<TextField> listOfTextFields = new ArrayList<>();
-        createTextFields(listOfTextFields);
+        ArrayList<TextField> listOfTextFields = createTextFields();
         createAndFillGridPane(addNewUserButton, listOfTextFields);
         addNewUserButton.setOnAction(event -> {
+            String login = listOfTextFields.get(2).getText();
+            if (isAnyFieldEmpty(listOfTextFields) && checkIfLoginIsUnique(login)) {
+                displayAcceptAlert(listOfTextFields);
+            }
 
+            anchor.requestFocus();
         });
-
     }
 
-    private void createTextFields(ArrayList<TextField> listOfAllFields) {
-        listOfAllFields = new ArrayList<>();
+    private void clearAllFields(ArrayList<TextField> listOfTextFields) {
+        for (var i : listOfTextFields) {
+            i.clear();
+        }
+    }
+
+    private boolean isAnyFieldEmpty(ArrayList<TextField> listOfTextFields) {
+        for (var i : listOfTextFields) {
+            if (i.getText().isEmpty()) {
+                createAndShowAlert(Alert.AlertType.WARNING, "Any field cannot be empty.", "New user", "Please fill all blanks and try again.");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void displayAcceptAlert(ArrayList<TextField> listOfTextFields) {
+        String name = listOfTextFields.get(0).getText();
+        String lastName = listOfTextFields.get(1).getText();
+        String login = listOfTextFields.get(2).getText();
+        String pass = listOfTextFields.get(3).getText();
+        String alertText = "User data \nname : " + name + "\nlast name : " + lastName + "\nlogin : " + login + "\npassword : " + pass;
+
+        Optional<ButtonType> button = createAndShowAlert(Alert.AlertType.CONFIRMATION, "Are you sure you want to add a new user?", "New user", alertText);
+        if (button.isPresent() && button.get() == ButtonType.OK) {
+            createAndShowConfirmAdminPasswordAlert("add new user", () -> {
+                Client client = new Client(login, name, lastName, pass);
+                client.addUserToDatabase(getConnection());
+                showNotification("User added successfully");
+                clearAllFields(listOfTextFields);
+                try {
+                    displayUsers();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+
+    private boolean checkIfLoginIsUnique(String login) {
+        try {
+            if (Client.isClientInDataBase(getConnection(), login)) {
+                createAndShowAlert(Alert.AlertType.WARNING, "A user with this login already exists", "Login", "Please choose a new login and try again");
+                return false;
+            }
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private ArrayList<TextField> createTextFields() {
+        ArrayList<TextField> listOfAllFields = new ArrayList<>();
         TextField userName = new TextField();
         userName.setPromptText("name");
         TextField userLastName = new TextField();
@@ -59,19 +112,18 @@ public class AllUserController extends AdminStartSceneController {
         listOfAllFields.add(userLastName);
         listOfAllFields.add(userLogin);
         listOfAllFields.add(userPassword);
+        return listOfAllFields;
     }
 
     private void createAndFillGridPane(Button addNewUserButton, ArrayList<TextField> fieldArrayList) {
         GridPane gridPane = new GridPane();
-        gridPane.setLayoutX(100);
-        gridPane.setLayoutY(500);
+        gridPane.setLayoutX(92);
+        gridPane.setLayoutY(565);
         anchor.getChildren().add(gridPane);
-
-
         gridPane.setHgap(30);
-        gridPane.add(fieldArrayList.get(0), 0, 0);
-        gridPane.add(fieldArrayList.get(1), 1, 0);
-        gridPane.add(fieldArrayList.get(2), 2, 0);
+        gridPane.add(fieldArrayList.get(2), 0, 0);
+        gridPane.add(fieldArrayList.get(0), 1, 0);
+        gridPane.add(fieldArrayList.get(1), 2, 0);
         gridPane.add(fieldArrayList.get(3), 3, 0);
         gridPane.add(addNewUserButton, 4, 0);
     }
@@ -87,8 +139,8 @@ public class AllUserController extends AdminStartSceneController {
     private void fillUserColumnsWithData(ObservableList<ClientTable> list) {
         userIDColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
         userLoginColumn.setCellValueFactory(new PropertyValueFactory<>("login"));
-        userLastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-        userFirstNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        userLastNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        userFirstNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
         userPasswordColumn.setCellValueFactory(new PropertyValueFactory<>("password"));
         userTableView.setItems(list);
         prepareTableView(userTableView, null);
